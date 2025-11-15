0fcd83b8814935e6ff0edb051d44d8f0e680e103
KillerBossOriginal <killerbossoriginal@outlook.it>
2025-11-15 14:30:25 +0100

    Segnata divide come unimplemented

--- src/numbers/number.rs
+++ src/numbers/number.rs
@@ -175,7 +175,9 @@ impl Number {
         }
     }
 
-    pub fn divide(&self, other: &Number, mut precision: usize) -> Number {
+    pub fn divide(&self, _other: &Number, mut _precision: usize) -> Number {
+        unimplemented!();
+        /* 
         let mut integer_result = vec![];
         let mut rational_result = vec![];
 
@@ -186,16 +188,20 @@ impl Number {
         other.shift_rationals_to_integers(other_rational_length);
         number.shift_rationals_to_integers(other_rational_length);
 
-        while rational_result.len() < precision {
-            let counter = 0;
-            unimplemented!();
-        }
+            let mut actual_part = Vec::new();
+            let mut counter = 0;
+            while !is_greater_than(&actual_part, &other.integer_part, true) {
+                actual_part.insert(0, number.positive_get_digit(counter, false));
+                counter += 1;
+            }
+
 
         Number {
             integer_part: integer_result,
             rational_part: rational_result,
             sign: Sign::Positive,
         }
+         */
     }
 
     pub fn shift_rational_to_integer(&mut self) {
